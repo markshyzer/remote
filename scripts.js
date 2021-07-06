3c276fa2d9ecefc2133720f6c4014e34bcc49084
Mark Shyzer <75265135+markshyzer@users.noreply.github.com>
2021-07-06 12:01:24 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,173 @@
+let remote = document.getElementById('remote')
+let code = 'AAAAAQAAAAEAAAAlAw=='
+
+
+
+
+remote.addEventListener('click', function(e) {
+    console.log("Clickety!", e.target.id)
+    command = e.target.id
+    if (command in code_list) {
+        sendCommand(command)
+    }
+
+})
+
+localStorage.setItem('IP','135.23.185.3');
+console.log(localStorage.getItem('IP'))
+
+function sendCommand(command) {
+    const req = new XMLHttpRequest()
+    const tv_url = 'http://135.23.185.3/sony/IRCC'
+    const preshared_key = '1qqa'
+    code = code_list[command]
+    req.open('POST', tv_url, true);
+
+    // req.onreadystatechange = function() {
+    //   if (req.readyState == 4) { // XMLHttpRequest.DONE
+    //     if (req.status != 200) {
+    //       // Safety: JSON.parse does not evaluate the attacker's scripts.
+    //       // const cleanedResponse = JSON.parse(req.responseText);
+    //       const message = 'Failed ' + command + ' ' + code + ' (Status '
+    //         + req.status + '): ' + tv_url + ' Key: ' + SONY_TV_PRESHARED_KEY
+    //         + ' (Response: "' + req.responseText + '")';
+    //       console.error(message);
+    //       reject(new Error(message));
+    //     } else {
+    //       // console.log('POST response received for: ' + command + ' ' + code);
+    //       resolve();
+    //     }
+    //   }
+    // }
+
+    req.setRequestHeader('Content-Type', 'text/xml; charset=UTF-8');
+    // Note: The SOAPAction header value must be enclosed in " otherwise get
+    // an Invalid Action error from TV!
+    req.setRequestHeader('SOAPAction', '"urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"');
+    req.setRequestHeader('X-Auth-PSK', preshared_key);
+    const data =
+      `<?xml version="1.0"?>
+      <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <s:Body>
+          <u:X_SendIRCC xmlns:u="urn:schemas-sony-com:service:IRCC:1">
+            <IRCCCode>${code}</IRCCCode>
+          </u:X_SendIRCC>
+        </s:Body>
+      </s:Envelope>`;
+
+    /* Other errors to watch for:
+       Incorrect IRCC code results in: 
+     HTTP/1.1 500 Internal Server Error
+     <errorCode>800</errorCode>
+     <errorDescription>Cannot accept the IRCC Code</errorDescription>
+     */
+
+    req.timeout = 3000; // in milliseconds
+    req.send(data);
+    console.log(`Sent ${command} command to TV`);
+    // Note: do not escape(data), that results in UPnPError:
+    //      <errorCode>401</errorCode>
+    //      <errorDescription>Invalid Action</errorDescription>
+
+}
+
+code_list = {
+    "PowerOff" : "AAAAAQAAAAEAAAAvAw==",
+    "Input" : "AAAAAQAAAAEAAAAlAw==",
+    "GGuide" : "AAAAAQAAAAEAAAAOAw==",
+    "EPG" : "AAAAAgAAAKQAAABbAw==",
+    "Favorites" : "AAAAAgAAAHcAAAB2Aw==",
+    "Display" : "AAAAAQAAAAEAAAA6Aw==",
+    "Home" : "AAAAAQAAAAEAAABgAw==",
+    "Options" : "AAAAAgAAAJcAAAA2Aw==",
+    "Return" : "AAAAAgAAAJcAAAAjAw==",
+    "Up" : "AAAAAQAAAAEAAAB0Aw==",
+    "Down" : "AAAAAQAAAAEAAAB1Aw==",
+    "Right" : "AAAAAQAAAAEAAAAzAw==",
+    "Left" : "AAAAAQAAAAEAAAA0Aw==",
+    "Confirm" : "AAAAAQAAAAEAAABlAw==",
+    "Red" : "AAAAAgAAAJcAAAAlAw==",
+    "Green" : "AAAAAgAAAJcAAAAmAw==",
+    "Yellow" : "AAAAAgAAAJcAAAAnAw==",
+    "Blue" : "AAAAAgAAAJcAAAAkAw==",
+    "Num1" : "AAAAAQAAAAEAAAAAAw==",
+    "Num2" : "AAAAAQAAAAEAAAABAw==",
+    "Num3" : "AAAAAQAAAAEAAAACAw==",
+    "Num4" : "AAAAAQAAAAEAAAADAw==",
+    "Num5" : "AAAAAQAAAAEAAAAEAw==",
+    "Num6" : "AAAAAQAAAAEAAAAFAw==",
+    "Num7" : "AAAAAQAAAAEAAAAGAw==",
+    "Num8" : "AAAAAQAAAAEAAAAHAw==",
+    "Num9" : "AAAAAQAAAAEAAAAIAw==",
+    "Num0" : "AAAAAQAAAAEAAAAJAw==",
+    "Num11" : "AAAAAQAAAAEAAAAKAw==",
+    "Num12" : "AAAAAQAAAAEAAAALAw==",
+    "VolumeUp" : "AAAAAQAAAAEAAAASAw==",
+    "VolumeDown" : "AAAAAQAAAAEAAAATAw==",
+    "Mute" : "AAAAAQAAAAEAAAAUAw==",
+    "ChannelUp" : "AAAAAQAAAAEAAAAQAw==",
+    "ChannelDown" : "AAAAAQAAAAEAAAARAw==",
+    "SubTitle" : "AAAAAgAAAJcAAAAoAw==",
+    "ClosedCaption" : "AAAAAgAAAKQAAAAQAw==",
+    "Enter" : "AAAAAQAAAAEAAAALAw==",
+    "DOT" : "AAAAAgAAAJcAAAAdAw==",
+    "Analog" : "AAAAAgAAAHcAAAANAw==",
+    "Teletext" : "AAAAAQAAAAEAAAA/Aw==",
+    "Exit" : "AAAAAQAAAAEAAABjAw==",
+    "Analog2" : "AAAAAQAAAAEAAAA4Aw==",
+    "*AD" : "AAAAAgAAABoAAAA7Aw==",
+    "Digital" : "AAAAAgAAAJcAAAAyAw==",
+    "Analog?" : "AAAAAgAAAJcAAAAuAw==",
+    "BS" : "AAAAAgAAAJcAAAAsAw==",
+    "CS" : "AAAAAgAAAJcAAAArAw==",
+    "BSCS" : "AAAAAgAAAJcAAAAQAw==",
+    "Ddata" : "AAAAAgAAAJcAAAAVAw==",
+    "PicOff" : "AAAAAQAAAAEAAAA+Aw==",
+    "Tv_Radio" : "AAAAAgAAABoAAABXAw==",
+    "Theater" : "AAAAAgAAAHcAAABgAw==",
+    "SEN" : "AAAAAgAAABoAAAB9Aw==",
+    "InternetWidgets" : "AAAAAgAAABoAAAB6Aw==",
+    "InternetVideo" : "AAAAAgAAABoAAAB5Aw==",
+    "Netflix" : "AAAAAgAAABoAAAB8Aw==",
+    "SceneSelect" : "AAAAAgAAABoAAAB4Aw==",
+    "Mode3D" : "AAAAAgAAAHcAAABNAw==",
+    "iManual" : "AAAAAgAAABoAAAB7Aw==",
+    "Audio" : "AAAAAQAAAAEAAAAXAw==",
+    "Wide" : "AAAAAgAAAKQAAAA9Aw==",
+    "Jump" : "AAAAAQAAAAEAAAA7Aw==",
+    "PAP" : "AAAAAgAAAKQAAAB3Aw==",
+    "MyEPG" : "AAAAAgAAAHcAAABrAw==",
+    "ProgramDescription" : "AAAAAgAAAJcAAAAWAw==",
+    "WriteChapter" : "AAAAAgAAAHcAAABsAw==",
+    "TrackID" : "AAAAAgAAABoAAAB+Aw==",
+    "TenKey" : "AAAAAgAAAJcAAAAMAw==",
+    "AppliCast" : "AAAAAgAAABoAAABvAw==",
+    "acTVila" : "AAAAAgAAABoAAAByAw==",
+    "DeleteVideo" : "AAAAAgAAAHcAAAAfAw==",
+    "PhotoFrame" : "AAAAAgAAABoAAABVAw==",
+    "TvPause" : "AAAAAgAAABoAAABnAw==",
+    "KeyPad" : "AAAAAgAAABoAAAB1Aw==",
+    "Media" : "AAAAAgAAAJcAAAA4Aw==",
+    "SyncMenu" : "AAAAAgAAABoAAABYAw==",
+    "Forward" : "AAAAAgAAAJcAAAAcAw==",
+    "Play" : "AAAAAgAAAJcAAAAaAw==",
+    "Rewind" : "AAAAAgAAAJcAAAAbAw==",
+    "Prev" : "AAAAAgAAAJcAAAA8Aw==",
+    "Stop" : "AAAAAgAAAJcAAAAYAw==",
+    "Next" : "AAAAAgAAAJcAAAA9Aw==",
+    "Rec" : "AAAAAgAAAJcAAAAgAw==",
+    "Pause" : "AAAAAgAAAJcAAAAZAw==",
+    "Eject" : "AAAAAgAAAJcAAABIAw==",
+    "FlashPlus" : "AAAAAgAAAJcAAAB4Aw==",
+    "FlashMinus" : "AAAAAgAAAJcAAAB5Aw==",
+    "TopMenu" : "AAAAAgAAABoAAABgAw==",
+    "PopUpMenu" : "AAAAAgAAABoAAABhAw==",
+    "RakurakuStart" : "AAAAAgAAAHcAAABqAw==",
+    "OneTouchTimeRec" : "AAAAAgAAABoAAABkAw==",
+    "OneTouchView" : "AAAAAgAAABoAAABlAw==",
+    "OneTouchRec" : "AAAAAgAAABoAAABiAw==",
+    "OneTouchStop" : "AAAAAgAAABoAAABjAw==",
+    "DUX" : "AAAAAgAAABoAAABzAw==",
+    "FootballMode" : "AAAAAgAAABoAAAB2Aw==",
+    "Social" : "AAAAAgAAABoAAAB0Aw=="
+    }
